69e9e358e08a508c1808c8474ebad8449c7c4bbe
deewakar <dakshftw01@gmail.com>
2025-02-23 14:37:42 +0530

    fix: build error

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,20 @@
 import { AnalyticsProvider } from "@/components/providers/posthog-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { Suspense } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <AnalyticsProvider>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
-    </AnalyticsProvider>
+    <Suspense fallback={null}>
+      <AnalyticsProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </AnalyticsProvider>
+    </Suspense>
   );
 }
